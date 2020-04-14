5c844ea316622f75a5ba833fd985fd6c01e6ffea
rae-glazier <glazier.home.life@gmail.com>
2020-04-14 18:29:08 -0500

    bum bum buuuuum

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express")
-
+const welcomeRouter = require("./routers/welcome-router")
 
 const server = express ()
 const port = 4000
 
 server.use(express.json())
-
-server.get("/", (req, res) => {
-    res.json({
-        message: "Node 2 API"
-    })
-})
+server.use("/", welcomeRouter)
 
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
