d666c1f5c011dfb4b3a6409d77d68cae76655bf8
rae-glazier <glazier.home.life@gmail.com>
2020-04-14 19:17:29 -0500

    errors and idk why

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const welcomeRouter = require("./routers/welcome-router")
+const postsRouter = require("./routers/posts-router")
 
 const server = express ()
 const port = 4000
 
 server.use(express.json())
 server.use("/", welcomeRouter)
+server.use("/api/posts", postsRouter)
 
 server.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`)
--- routers/posts-router.js
+++ routers/posts-router.js
@@ -1,2 +1,25 @@
 const express = require("express")
 const router = express.Router
+const posts = require("../data/db")
+
+//GET endpoints
+
+router.get("/", (req,res) => {
+    const options = {
+        sortBy: req.query.sortBy,
+        limit: req.query.limit,
+    }
+
+    posts.find(options)
+        .then((posts) =>{
+            res.status(200).json(posts)
+        })
+        .catch(() => {
+            console.log(error)
+			res.status(500).json({
+				error: "The posts information could not be retrieved.",
+			})
+        })
+})
+
+module.exports = router
