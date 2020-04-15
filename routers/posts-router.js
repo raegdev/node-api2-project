45359877079afe601230edb0b78203f43383a0e7
rae-glazier <glazier.home.life@gmail.com>
2020-04-14 20:29:20 -0500

    specific posts comments get works

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -38,4 +38,24 @@ router.get("/:id", (req,res) => {
         })
 })
 
+router.get("/:id/comments", (req,res) => {
+
+    posts.findPostComments(req.params.id)
+        .then((posts) => {
+            if(posts) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({
+                    message: "the posts with the specified ID does not exist"
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "error retrieving the posts comments"
+            })
+        })
+})
+
 module.exports = router
