6c04b5f36c9b70722bceb65694ab45f69cd667b8
rae-glazier <glazier.home.life@gmail.com>
2020-04-14 20:16:54 -0500

    get post by id works

--- routers/posts-router.js
+++ routers/posts-router.js
@@ -18,4 +18,24 @@ router.get("/", (req,res) => {
         })
 })
 
+router.get("/:id", (req,res) => {
+
+    posts.findById(req.params.id)
+        .then((posts) => {
+            if (posts) {
+                res.status(200).json(posts)
+            } else {
+                res.status(404).json({
+                    message: "the posts with the specified ID does not exist"
+                })
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+            res.status(500).json({
+                message: "error retrieving the posts"
+            })
+        })
+})
+
 module.exports = router
